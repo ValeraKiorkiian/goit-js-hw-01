ebd88d34379105ed1253176e28babd8b1b4b7af2
Valerii Dzhurenko-Kiorkiian <valerakiorkian@gmail.com>
2024-11-30 13:38:59 -0500

    Update task-3.js

--- js/task-3.js
+++ js/task-3.js
@@ -1,7 +1,7 @@
 function getElementWidth(content, padding, border) {
   const value = content + padding * 2 + border * 2;
-  const sum = Number.parseFloat(value);
-  return `${sum}px`;
+  const sum = parseFloat(value);
+  return sum;
 }
 getElementWidth('50px', '8px', '4px');
 getElementWidth('60px', '12px', '8.5px');
